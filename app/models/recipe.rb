1575fe9e7d027022bc9ceb79907b79203fc329ba
bwalker8038 <bwalker8038@gmail.com>
2011-02-13 16:04:56 -0500

    updated models

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,2 +1,8 @@
 class Recipe < ActiveRecord::Base
+  
+  validates :title, :presence => true
+  validates :ingredients, :presence => true
+  validates :disc, :presence => true
+
+  belongs_to :user
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,13 @@
 class User < ActiveRecord::Base
+  validates :username, :presence => true, 
+                       :uniqueness => true,
+  
+  validates :password, :confirmation => true
+
+  validates :email, :uniqueness => true,
+                    :length => { :within => 5..100 },
+                    :format => { :with => /^[^@][\w.-]+@[\w.-]+[.][a-z]{2,4}$/i }
+
+  has_many :recipes, :order => 'title ASC',
+                     :dependent => :nullify
 end

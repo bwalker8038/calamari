c99182d8939cfe166cfc496174b9494884e840d0
bwalker8038 <bwalker8038@gmail.com>
2011-02-12 21:15:00 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Calamari::Application.config.secret_token = '970755ca9dd2508f6599252787bce649ef47807aeea5a61b009042b172c1ab1f96ea5c45a1e1bada28a9d9aa478123acaa321a88a7477334edda2db4e5152389'

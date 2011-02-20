6c83d021ea0e2b89fad1dcf8c1ffbb1a42e1ad1d
bwalker8038 <bwalker8038@gmail.com>
2011-02-20 13:15:32 -0500

    added jquery support

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,4 +4,3 @@ module ApplicationHelper
   def submit_or_cancel(form, name='Cancel')
     link_to(name, 'javascript:history.go(-1);', :class => 'cancel')
   end
-end
--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,5 +1,2 @@
 module UsersHelper
-  def submit_or_cancel(form, name='Cancel')
-    link_to(name, 'javascript:history.go(-1);', :class => 'cancel')
-  end
 end
